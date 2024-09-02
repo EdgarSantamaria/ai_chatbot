db767fcdcc6c990f2002d5e2dbe8279b33c18ea4
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-09-02 12:49:31 -0400

    fetch fix

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
     console.log("Sending Query:", query);
     
     try {
-      const response = await fetch('https://youtube-chatbot.vercel.app/perform_rag', {
+      const response = await fetch('https://api-rag-five.vercel.app/perform_rag', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ youtube_url: youtubeUrl, query: query }),
